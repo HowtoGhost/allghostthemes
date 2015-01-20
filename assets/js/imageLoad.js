e75bd97571a9c58b3794cc3013576b88eb112697
David Balderston <dbalders@gmail.com>
2015-01-19 22:00:53 -0800

    Was Missing a Few Lines

--- assets/js/imageLoad.js
+++ assets/js/imageLoad.js
@@ -2,9 +2,9 @@
 $(window).load(function() {
 	var tableTitle = $('.tableImage');
 	var imageTitle = $('.tableTitle a');
-
+	var i = 0;
+	
 	for (i; i < tableTitle.length; i++) {
 		$('.tableImage')[i].innerHTML = "<a href=''><img src='http://cdn.allghostthemes.com/assets/images/" + imageTitle[i].innerText + ".jpg' /></a>";
-		// console.log(imageTitle[i].innerText);
 	}
 });
