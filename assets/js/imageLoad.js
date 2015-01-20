d5b26b2416651e645dc982ebd6002a09da9f819d
David Balderston <dbalders@gmail.com>
2015-01-19 21:30:16 -0800

    Loading Images After Page Load

--- assets/js/imageLoad.js
+++ assets/js/imageLoad.js
@@ -1,12 +1,10 @@
 //Loads images and then initializes Datatables
-$(document).ready(function() {
+$(window).load(function() {
 	var tableTitle = $('.tableImage');
 	var imageTitle = $('.tableTitle a');
 
 	for (i; i < tableTitle.length; i++) {
-		// imageTitle = $('.tableTitle a').innerHTML;
 		$('.tableImage')[i].innerHTML = "<a href=''><img src='http://cdn.allghostthemes.com/assets/images/" + imageTitle[i].innerText + ".jpg' /></a>";
 		// console.log(imageTitle[i].innerText);
-		// console.log(tableTitle[i].innerText);
 	}
 });
