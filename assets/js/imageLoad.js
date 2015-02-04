945beb4f5430b53cb279fa957d3f0fa8ab84a4ba
David Balderston <dbalders@gmail.com>
2015-02-03 18:43:51 -0800

    Fix Layout on Tag Pages and Individual Listings

--- assets/js/imageLoad.js
+++ assets/js/imageLoad.js
@@ -3,8 +3,10 @@ $(window).load(function() {
 	var tableTitle = $('.tableImage');
 	var imageTitle = $('.tableTitle a');
 	var i = 0;
+
 	
 	for (i; i < tableTitle.length; i++) {
-		$('.tableImage')[i].innerHTML = "<a href=''><img src='http://cdn.allghostthemes.com/assets/images/" + imageTitle[i].innerText + ".jpg' /></a>";
+		var imageLink = imageTitle[i].innerText.toLowerCase();
+		$('.tableImage')[i].innerHTML = "<a href='/" + imageLink + "/'" + "><img src='http://cdn.allghostthemes.com/assets/images/" + imageTitle[i].innerText + ".jpg' /></a>";
 	}
 });
